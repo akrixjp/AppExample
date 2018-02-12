1a245022eda3e68778269200c554eb9e6c086fac
Jorge <jorgegpinedo26@gmail.com>
2018-02-11 19:26:51 -0500

    JPinedo Commit

--- App_05_02/app/src/main/java/com/example/jeffry/app_05_02/MainActivity.java
+++ App_05_02/app/src/main/java/com/example/jeffry/app_05_02/MainActivity.java
@@ -11,5 +11,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+        /*
+        * JPINEDO CAMBIO
+        *
+        * */
+
+
     }
 }
